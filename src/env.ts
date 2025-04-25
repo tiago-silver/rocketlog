d2b77530e44e0b7e1f7614a30cc78bc97988fe94
Tiago Silva <tiagopereirasilva5425@gamil.com>
2025-04-25 17:52:21 -0300

    chore: deploy config

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,9 @@ import { z } from "zod"
 
 const envSchema = z.object({
     DATABASE_URL: z.string().url(),
-    JWT_SECRET: z.string()
+    JWT_SECRET: z.string(),
+    //Caso retorne uma string, converta para número
+    PORT : z.coerce.number().default(3000)
 })
 
 export const env = envSchema.parse(process.env)
